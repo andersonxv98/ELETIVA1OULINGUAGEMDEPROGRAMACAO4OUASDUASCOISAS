5077a8b3d8da4d0337afc29ae3970517b37c3e68
andiSu <anderson_rockX@hotmail.com>
2022-03-04 21:24:18 -0300

    separação php do html

--- aula2502/resposta.php
+++ aula2502/resposta.php
@@ -12,29 +12,8 @@
 </head>
 <body>
 <h1>calculo</h1>
-<?php
-    $var1 = $_POST['valor1'];
-    $var2 = $_POST['valor2'];
-    $result = [($var1 + $var2),($var1 - $var2),($var1 * $var2),($var1 / $var2)];
-    echo "soma, sub, mult, div:  ".Implode($result);
-    echo "<br/>";
-    echo "soma: ".$result[0];
-echo "<br/>";
-    echo "sub: ".$result[1];
-echo "<br/>";
-    echo "mult: ".$result[2];
-echo "<br/>";
-    echo "div: ".$result[3];
-echo "<br/>";
-    if ($var1 % 2 == 0){
-        echo "é par!!";
-    }
-    else{
-        echo "impar";
-    }
+<?php require 'lolgica.php' ?>
 
-
-?>
 <!-- Optional JavaScript; choose one of the two! -->
 
 <!-- Option 1: Bootstrap Bundle with Popper -->
