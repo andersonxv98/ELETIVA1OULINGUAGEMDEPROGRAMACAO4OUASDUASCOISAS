017971dd1ef5bc827592d16193bae273e18506e2
Anderson Olveira <anderson_rockx@hotmail.com>
2022-03-24 14:21:56 -0300

    test
    3

--- ListaExer2/exer1.php
+++ ListaExer2/exer1.php
@@ -1,37 +1,22 @@
 <?php
 
-function CalcularIMC($var1, $var2){
-    $imc = $var1 / ($var2 * $var2);
-    switch ($imc) {
-        case ($imc < 18.5):
-            echo "Abaixo do peso";
-            break;
-        case ($imc >= 18.6) && ($imc <= 24.9):
-            echo "Peso ideal! parabens seu gostosao";
-            break;
-        case ($imc >= 25) && ($imc <= 29.9):
-            echo "Peso levemente acima da média";
-                break;
-        case ($imc >= 30) && ($imc <= 34.9):
-            echo "Obesidade grau 1";
-                break;
-        case ($imc >= 35) && ($imc <= 39.9):
-            echo "Obesidade Grau 2 ";
-                break;
-        case ($imc >= 40):
-            echo "Obesidade Grau 3, olha a motinha do pai VRUMMMMMMMMMMM ";
-                break;
-        default:
-            # code...
-            break;
+for ($i=1; $i<=10; $i++){
+    $vetor[$i] = $_POST["valor$i"];
+}
+
+$posicao = 0;
+$maior = 0;
+//var_dump($vetor);
+foreach($vetor as $chave => $valor){
+    echo "Posição do Vetor: $chave e seu valor é $valor";
+    echo "<br>";
+    if ($valor > $maior){
+        $maior = $valor;
+        $posicao = $chave;
     }
 }
 
+echo "O maior valor é $maior e está na posição $posicao";
 
-
-$var1 = $_POST['valor1']; 
-$var2 = $_POST['valor2'];
-
-CalcularIMC($var1, $var2);
 
 ?>
