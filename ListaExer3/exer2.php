29ffc10d4c35ef954d0f28f228b74d225aac6dbc
Anderson Olveira <anderson_rockx@hotmail.com>
2022-03-24 11:07:21 -0300

    e
    w

--- ListaExer3/exer2.php
+++ ListaExer3/exer2.php
@@ -10,9 +10,17 @@ function Lernomes($vet_alunos){
 }
 
 $var1 = $_POST['valor1']; //valor kilo
-array_push($vet_alunos, $var1);
+$var2 = $_POST['valor2'];
+$var3 = $_POST['valor3'];
+$var4 = $_POST['valor4'];
+$var5 = $_POST['valor5'];
+$var6 = $_POST['valor6'];
+$var7 = $_POST['valor7'];
+$var8 = $_POST['valor8'];
+$var9 = $_POST['valor9'];
+$var10 = $_POST['valor10'];
+array_push($vet_alunos, $var1, $var2,$var3,$var4,$var5,$var6,$var7,$var8,$var9,$var10);
 
-if (count($vet_alunos) >= 10){
-    Lernomes($vet_alunos);
-}
+
+Lernomes($vet_alunos)
 ?>
