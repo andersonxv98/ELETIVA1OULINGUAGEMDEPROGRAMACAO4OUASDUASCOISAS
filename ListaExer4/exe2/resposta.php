d1bfcea5d5882c70f2d913313e51cbb2cf5a664a
Anderson Olveira <anderson_rockx@hotmail.com>
2022-03-31 10:10:10 -0300

    mais confuso que o cunfuchio
    2

---
+++ ListaExer4/exe2/resposta.php
@@ -0,0 +1,77 @@
+
+   
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <title>Sistyema em PHP</title>
+  </head>
+  <body>
+    <h1>Sistema PHP</h1>
+    <?php 
+        if($_POST){
+        $email = $_POST['email'];
+        $senha = $_POST['senha'];
+        $tempo_volta = $_POST['tempo_volta'];
+      /*  setcookie("usuario", $email, time() + (86400 * 1), "/");
+        echo "SEja BEm venindo: ".$_COOKIE['usuario'];*/
+
+        session_start();
+            if(($email == "andisu@andisu.com") && ($senha==123)){
+                $_SESSION['usuario'] = $email;
+                $_SESSION['acesso'] = true;
+                if(isset($_SESSION['vet_tempvolta'])){
+                  $_SESSION['vet_tempvolta'].=','.$tempo_volta;
+                }
+                else{
+                  $_SESSION['vet_tempvolta']=$tempo_volta;
+                }
+                $vet_voltas = explode(',', $_SESSION['vet_tempvolta']);
+                $min_valor =min($vet_voltas);
+                
+                foreach ($vet_voltas as $key => $value) {
+                  if($value == $min_valor){
+                    $_indice =$key;
+                  }
+                }
+
+                echo "seja bem vindo usuario: ".$_SESSION['usuario'];
+                echo "</br>";
+                echo "Todos os Tempos: ".$_SESSION['vet_tempvolta'];
+                echo "</br>";
+                echo "Melhor Valor(menor tempo ): ".$min_valor;
+                echo "</br>";
+                echo "volta N em que o valor foi registrado".$_indice+1;
+                echo "</br>";
+                echo "<a href='sair.php'>SAIR</a>";
+                
+            }
+            else{
+                header('Location: index.php');
+                exit;
+            }
+        }
+        else{
+            session_start();
+            if($_SESSION["acesso"] == false){
+                header('Location: index.php');
+                exit;
+
+            }
+        }
+    ?>
+
+    <!-- Optional JavaScript; choose one of the two! -->
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
